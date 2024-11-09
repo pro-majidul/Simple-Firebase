f5555e166527e689bbf59c4801bc2a80567fb05e
Majidul Islam <160348093+pro-majidul@users.noreply.github.com>
2024-11-09 18:03:02 +0600

    daynamic login and logout added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,48 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from "../firebase/Firebase.init";
+import { useState } from "react";
 
 const Login = () => {
 
     const provider = new GoogleAuthProvider();
-    
-    const handelGoogleSignIn = () =>{
-        signInWithPopup(auth , provider)
-        .then((result)=>{
-            console.log(result);
-        })
-        .catch(error =>{
-            console.log('error' , error);
-        })
+    const [users, setUsers] = useState(null)
+
+    const handelGoogleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                console.log(result.user)
+                setUsers(result.user)
+            })
+            .catch(error => {
+                console.log('error', error);
+                setUsers(null)
+            })
     }
+    const handelSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result);
+                setUsers(null)
+            })
+            .catch(error => console.log(error))
+    }
+
+
 
     return (
         <div>
-            <button onClick={handelGoogleSignIn}>Login with Google</button>
+           {
+            users ?  
+            <button onClick={handelSignOut}>Sign Out</button> :<button onClick={handelGoogleSignIn}>Login with Google</button>
+           }
+
+            {
+                users && <div>
+                    <h3> UserName : {users.displayName}</h3>
+                    <p>Email : {users.email}</p>
+                    <img src={users.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
